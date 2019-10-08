14e7f3a26830644e3f663d587b5384ac8f922d4b
jkneisler1 <John.Kneisler@gmail.com>
2019-10-08 11:49:47 -0400

    Changed the DBMS from H2 to MySQL

--- src/main/java/com/example/demo/CourseRepository.java
+++ src/main/java/com/example/demo/CourseRepository.java
@@ -2,4 +2,5 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface CourseRepository extends CrudRepository<Course, Long> {}
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
